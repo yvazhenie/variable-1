353c4456aa6b0b31fabc6fb3af63f55682a00840
Makarov <moeyva@yandex.ru>
2022-12-20 20:04:30 +0300

    qw 1-3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hw");
+        var dog = 8.0;
+        System.out.println (dog);
+        var cat = 3.6;
+        System.out.println (cat);
+        var paper = 763789;
+        System.out.println (paper);
+        dog = dog + 4;
+        System.out.println(dog);
+        cat = cat + 4;
+        System.out.println(cat);
+        paper = paper + 4;
+        System.out.println(paper);
+        dog = dog - 3.5;
+        System.out.println(dog);
+        cat = cat - 1.6;
+        System.out.println(cat);
+        paper = paper - 7639;
+        System.out.println(paper);
     }
 }
